6dd28c43abbf60bbe0713a51228616f3088e675b
Aldrin <aamistoso10@gmail.com>
2023-08-24 14:15:56 -0700

    Finished modal and added contrast mode

---
+++ index.js
@@ -0,0 +1,50 @@
+// EmailJS
+// template_fsf4o8m
+// service_55toxca
+// _KyDg9uKgvsqdacZp
+
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+function contact() {
+    event.preventDefault();  // so it doesn't refresh the page when clicking submit
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+      .sendForm(
+        'service_55toxca',
+        'template_fsf4o8m',
+        event.target,
+        '_KyDg9uKgvsqdacZp'
+      ).then(() => {
+        loading.classList.remove("modal__overlay--invisible")
+        success.classList += " modal__overlay--visible"
+      }).catch(() => {
+        loading.classList.remove("modal__overlay--invisible")
+        alert(
+            "The email service is temprarily unavailable. Please contact me directly on aamistoso10@gmail.com"
+        )
+      })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
+    document.body.classList += " modal--open"
+    
+}
+
